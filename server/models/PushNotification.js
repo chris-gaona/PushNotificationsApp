46fc2d3022640d3f59eaab878dfee7b85ae9e3af
Chris Gaona <chrisgaona@Chriss-STS-MacBook-Pro.local>
2019-04-19 09:56:56 -0700

    Wires up sending bulk push notifications

--- server/models/PushNotification.js
+++ server/models/PushNotification.js
@@ -1,4 +1,4 @@
-const Expo = require("expo-server-sdk");
+const { Expo } = require("expo-server-sdk");
 const db = require("../db");
 
 const dbKey = "pushNotifications";
@@ -24,7 +24,36 @@ const addPushToken = ({ token, platform, timezoneOffset }) => {
     });
 };
 
+const sendNewNotificationToAll = ({ questions, nextQuestionTime }) => {
+  const expo = new Expo();
+  return db
+    .table(dbKey)
+    .then(docs => {
+      const messages = [];
+      docs.forEach(doc => {
+        messages.push({
+          to: doc.token,
+          sound: "default",
+          body: questions[0].question
+        });
+      });
+
+      return { messages };
+    })
+    .then(({ messages }) => {
+      const messageChunks = expo.chunkPushNotifications(messages);
+
+      const expoRequests = messageChunks.map(chunk => {
+        return expo.sendPushNotificationsAsync(chunk);
+      });
+
+      return Promise.all(expoRequests);
+    })
+    .then(res => console.log(res));
+};
+
 module.exports = {
   dbKey,
-  addPushToken
+  addPushToken,
+  sendNewNotificationToAll
 };
--- server/util/jobs.js
+++ server/util/jobs.js
@@ -1,15 +1,29 @@
 const schedule = require("node-schedule");
 const Question = require("../models/Question");
+const PushNotifications = require("../models/PushNotification");
 
 const createNewQuestionJob = () => {
   // Run at the top of every hour
   const scheduleRule = "0 * * * *";
   const job = schedule.scheduleJob(scheduleRule, () =>
-    Question.setNewQuestion()
+    Question.setNewQuestion().then(questions => {
+      return PushNotifications.sendNewNotificationToAll({
+        questions,
+        nextQuestionTime: job.nextInvocation()
+      });
+    })
   );
 
   return job;
 };
+
+const job = schedule.scheduleJob("0 * * * *", () => null);
+Question.setNewQuestion().then(questions => {
+  return PushNotifications.sendNewNotificationToAll({
+    questions,
+    nextQuestionTime: job.nextInvocation()
+  });
+});
 
 module.exports = {
   createNewQuestionJob
